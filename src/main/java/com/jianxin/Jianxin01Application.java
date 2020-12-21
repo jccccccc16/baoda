c01410f33e4bbe9bedf006750823d0028640fed8
jccccccc16 <617028197@qq.com>
2020-12-21 15:52:55 +0800

    创建项目

---
+++ src/main/java/com/jianxin/Jianxin01Application.java
@@ -0,0 +1,13 @@
+package com.jianxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jianxin01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jianxin01Application.class, args);
+    }
+
+}
